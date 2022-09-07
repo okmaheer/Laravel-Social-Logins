3643b620a76330c0c5c7149e075235107a188b71
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-09-07 23:08:38 +0500

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,7 +14,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactor y, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
