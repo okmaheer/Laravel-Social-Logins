4934ac80cd65380836f6820c5d91803f5471853e
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-09-13 09:38:33 +0500

    Update create.blade.php

--- resources/views/dashboard/accounts/roles/create.blade.php
+++ resources/views/dashboard/accounts/roles/create.blade.php
@@ -55,7 +55,7 @@
                         <div class="row">
                             <div class="col-md-12 bg-soft-primary text-center">
                                 <h3 class="my-1">Permissions</h3>
-                            </div>
+                            </div
                         </div>
                         <div class="row mt-2">
                             <div class="col-md-12 text-center">
