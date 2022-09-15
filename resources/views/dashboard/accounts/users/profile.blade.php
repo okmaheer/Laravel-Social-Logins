ed5ecb874342d3b61b2f96aacc1deda88cb8975b
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-09-15 16:59:13 +0500

    Update profile.blade.php

--- resources/views/dashboard/accounts/users/profile.blade.php
+++ resources/views/dashboard/accounts/users/profile.blade.php
@@ -11,7 +11,7 @@
     <div class="page-content">
         <div class="container-fluid">
 
-
+ 
             <div class="row">
                 <div class="col-lg-3">
                     <div class="card">
